d409bd2539ef3445091e844b304d65b1681dd172
CamBinh <binhvtc3103@gmail.com>
2023-10-06 16:57:03 +0700

    add user

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='pb-5 border-bottom'>
+        <h1 className='text-center' to ="/user-page">USER PAGE</h1>
+      </div>
+    ) 
+  }
+}
